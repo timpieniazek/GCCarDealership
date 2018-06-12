3adc11ed46363650a108528edfe3ce9efbfcb2e5
Tim Pieniazek <timpieniazek@gmail.com>
2018-06-12 09:51:11 -0400

    Hello World

--- GCCarDealership/src/main/java/com/gc/GCCarDealership/controller/CarController.java
+++ GCCarDealership/src/main/java/com/gc/GCCarDealership/controller/CarController.java
@@ -1,5 +1,13 @@
 package com.gc.GCCarDealership.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class CarController {
 
+	@RequestMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
+	
 }
