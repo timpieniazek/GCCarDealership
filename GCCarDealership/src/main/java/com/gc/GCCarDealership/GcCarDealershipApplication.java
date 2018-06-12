a329c1abc052990115bf6bf4d0998b84f7f71e19
Tim Pieniazek <timpieniazek@gmail.com>
2018-06-12 09:38:46 -0400

    Initial commit

---
+++ GCCarDealership/src/main/java/com/gc/GCCarDealership/GcCarDealershipApplication.java
@@ -0,0 +1,12 @@
+package com.gc.GCCarDealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcCarDealershipApplication.class, args);
+	}
+}
