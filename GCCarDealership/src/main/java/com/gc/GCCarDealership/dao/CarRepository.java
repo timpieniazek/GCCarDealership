9e6bd4e7e522db4440dd41f8d5dab85c59def99d
reakoffa <reakoffa@gmail.com>
2018-06-12 10:10:25 -0400

    pojo and repository classes

--- GCCarDealership/src/main/java/com/gc/GCCarDealership/dao/CarRepository.java
+++ GCCarDealership/src/main/java/com/gc/GCCarDealership/dao/CarRepository.java
@@ -1,5 +1,15 @@
 package com.gc.GCCarDealership.dao;
 
-public interface CarRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gc.GCCarDealership.entity.Car;
+
+public interface CarRepository extends JpaRepository<Car, Integer> {
+
+	List<Car> findByMake(String make);
+	List<Car> findByModel(String model);
+	List<Car> findByYear(String year);
+	List<Car> findByColor(String color);
 }
--- GCCarDealership/src/main/java/com/gc/GCCarDealership/entity/Car.java
+++ GCCarDealership/src/main/java/com/gc/GCCarDealership/entity/Car.java
@@ -1,5 +1,57 @@
 package com.gc.GCCarDealership.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String make;
+	private String model;
+	private String year;
+	private String color;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public Car() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Car [id=" + id + ", make=" + make + ", model=" + model + ", year=" + year + ", color=" + color + "]";
+	}
+	
 }
