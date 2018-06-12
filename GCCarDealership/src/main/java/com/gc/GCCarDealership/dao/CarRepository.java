d94040942a44808bd11bed3067b02e68682a7cd2
reakoffa <reakoffa@gmail.com>
2018-06-12 10:33:16 -0400

    completed repository

--- GCCarDealership/src/main/java/com/gc/GCCarDealership/dao/CarRepository.java
+++ GCCarDealership/src/main/java/com/gc/GCCarDealership/dao/CarRepository.java
@@ -12,4 +12,20 @@ public interface CarRepository extends JpaRepository<Car, Integer> {
 	List<Car> findByModel(String model);
 	List<Car> findByYear(String year);
 	List<Car> findByColor(String color);
+	
+	List<Car> findByMakeAndModel(String make, String model);
+	List<Car> findByMakeAndYear(String make, String year);
+	List<Car> findByMakeAndColor(String make, String color);
+	List<Car> findByMakeModelAndYear(String make, String model, String year);
+	List<Car> findByMakeModelAndColor(String make, String model, String color);
+	List<Car> findByMakeYearAndColor(String make, String year, String color);
+	List<Car> findByMakeModelYearAndColor(String make, String model, String year, String color);
+	
+	List<Car> findByModelAndYear(String model, String year);
+	List<Car> findByModelAndColor(String model, String color);
+	List<Car> findByModelYearAndColor(String model, String year, String color);
+	
+	List<Car> findByYearAndColor(String year, String color);
+	
+	
 }
